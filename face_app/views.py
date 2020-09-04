382942b6ecbab2281d4d562e236298d79d12de5a
Prajval sudhir <prajvalsudhir@gmail.com>
2020-09-04 22:30:37 +0530

    Update views.py

--- face_app/views.py
+++ face_app/views.py
@@ -8,6 +8,9 @@ from .forms import input_form
 from .models import face_photos,predicted_photos
 
 from .face_model import predict,predict_v2
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Create your views here.
 
@@ -21,8 +24,11 @@ def user_input(requests):
             # face_photo = face_photos()
             # face_photo.img_input = 'images_input/' + str(form.cleaned_data['img_input'])
             # face_photo.save()
-            input_path = 'C:\\Users\\prajv\\Desktop\\PycharmProjects\\PS-PY\\venv\\djenv\\face_pro\\media\\images_input\\' + str(form.cleaned_data['img_input'])
-            output_path = 'C:\\Users\\prajv\\Desktop\\PycharmProjects\\PS-PY\\venv\\djenv\\face_pro\\media\\images_output\\'  + str(form.cleaned_data['img_input'])
+            #input_path = 'C:\\Users\\prajv\\Desktop\\PycharmProjects\\PS-PY\\venv\\djenv\\face_pro\\media\\images_input\\' + str(form.cleaned_data['img_input'])
+            #output_path = 'C:\\Users\\prajv\\Desktop\\PycharmProjects\\PS-PY\\venv\\djenv\\face_pro\\media\\images_output\\'  + str(form.cleaned_data['img_input'])
+            input_path = os.path.join(BASE_DIR,'media/images_input/') + str(form.cleaned_data['img_input'])
+            output_path = os.path.join(BASE_DIR,'media/images_output/') + str(form.cleaned_data['img_input'])
+            
             pro_img,count,with_mask,without_mask = predict_v2(input_path,output_path,form)
             return render(requests,'face_app/result.html',{'processed_img':pro_img , 'count':count,'with_mask':with_mask,'without_mask':without_mask})
             # return HttpResponse('image uploaded')
